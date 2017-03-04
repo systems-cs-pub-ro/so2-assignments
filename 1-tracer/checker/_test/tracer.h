8de7cb4b5a337fd255670ed374cfdfd7a68b29c3
Adrian Stanciu <adrian.stanciu.pub@gmail.com>
2017-03-04 15:09:54 +0200

    add 1-tracer checker and util

---
+++ 1-tracer/checker/_test/tracer.h
@@ -0,0 +1,22 @@
+/*
+ * SO2 kprobe based tracer header file
+ * 2013, Operating Systems 2 - Ixia Challenge
+ *
+ * this is shared with user space
+ */
+
+#ifndef TRACER_H__
+#define TRACER_H__ 1
+
+#include <asm/ioctl.h>
+#ifndef __KERNEL__
+#include <sys/types.h>
+#endif /* __KERNEL__ */
+
+#define TRACER_DEV_MINOR 42
+#define TRACER_DEV_NAME "tracer"
+
+#define TRACER_ADD_PROCESS	_IOW(_IOC_WRITE, 42, pid_t)
+#define TRACER_REMOVE_PROCESS	_IOW(_IOC_WRITE, 43, pid_t)
+
+#endif /* TRACER_H_ */
---
+++ 1-tracer/checker/_test/tracer_test.h
@@ -0,0 +1,27 @@
+/*
+ * SO2 Kprobe based tracer - test suite specific header
+ * 2013, Operating Systems 2 - Ixia Challenge
+ *
+ * Authors:
+ *	Daniel Baluta <daniel.baluta@gmail.com>
+ */
+
+#ifndef TRACER_TEST_H_
+#define TRACER_TEST_H_		1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* tracer test suite macros and structures */
+#define MODULE_NAME		"tracer"
+#define MODULE_FILENAME		MODULE_NAME ".ko"
+
+#define HELPER_MODULE_NAME	"tracer_helper"
+#define HELPER_MODULE_FILENAME	HELPER_MODULE_NAME ".ko"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
